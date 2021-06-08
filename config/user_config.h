7cdd74af00d8b665ad6e99f16e0b41b112618d32
kaitlynng <Kaitlyn.nky@gmail.com>
2021-06-08 10:34:13 +0800

    basic script to test aws mqtt functionality

--- config/user_config.h
+++ config/user_config.h
@@ -6,8 +6,8 @@
 #define CONTRACT_NAME   "TENGAHPAC07/D/027"
 
 // network credentials
-#define NETWORK_SSID    "AIDrivers SG - 2.4G" // "Ng-fam-weefee-extraordinaire" // "AndroidAPf2b2" // "HDB4" //"mobilelogger@95";//"AIDrivers SG - 2.4G";//"@zfzenryu";//AIDrivers SG - 5G//SINGTEL-776B//AndroidAPf2b2//mobilelogger95//SPSS_AQC601//HDB2//HDB3//TLC
-#define NETWORK_PASS    "@1Drivers75" // "tecknam67941827" // "rfkv0877" // "hdbdatalogger" //"logger@95";//"@1Drivers75";//"Zengfu9595";//@1Drivers75//eequuughah//rfkv0877//logger95//SPSS_AQC601//hdbdatalogger//TLC68620769
+#define NETWORK_SSID    "Ng-fam-weefee-extraordinaire" // "AIDrivers SG - 2.4G" // "AndroidAPf2b2" // "HDB4" //"mobilelogger@95";//"AIDrivers SG - 2.4G";//"@zfzenryu";//AIDrivers SG - 5G//SINGTEL-776B//AndroidAPf2b2//mobilelogger95//SPSS_AQC601//HDB2//HDB3//TLC
+#define NETWORK_PASS    "tecknam67941827" // "@1Drivers75" // "rfkv0877" // "hdbdatalogger" //"logger@95";//"@1Drivers75";//"Zengfu9595";//@1Drivers75//eequuughah//rfkv0877//logger95//SPSS_AQC601//hdbdatalogger//TLC68620769
 
 const int LOCAL_IP_ADDRESS[] = {192, 168, 10, 184}; 
 const int GATEWAY_ADDRESS[] = {192, 168, 10, 1}; // need to configure!
